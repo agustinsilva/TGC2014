c051cc675daacccb6143937f8e083f18c168f177
Agustin Silva <agustin.silva@live.com>
2014-08-31 14:22:31 -0300

    comienzo de TGC

---
+++ TgcViewer/AlumnoEjemplos/PiratasEspaciales/EjemploAlumno.cs
@@ -0,0 +1,225 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TgcViewer.Example;
+using TgcViewer;
+using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using Microsoft.DirectX;
+using TgcViewer.Utils.Modifiers;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.Sound;
+using System.IO;
+
+namespace AlumnoEjemplos.MiGrupo
+{
+    /// <summary>
+    /// Ejemplo del alumno
+    /// </summary>
+    public class EjemploAlumno : TgcExample
+    {
+        string currentFile;
+
+        readonly Vector3 SUN_SCALE = new Vector3(12, 12, 12);
+        const float AXIS_ROTATION_SPEED = 0.5f;
+        float axisRotation = 0f;
+        TgcMesh sol;
+        /// <summary>
+        /// Categoría a la que pertenece el ejemplo.
+        /// Influye en donde se va a haber en el árbol de la derecha de la pantalla.
+        /// </summary>
+        public override string getCategory()
+        {
+            return "AlumnoEjemplos";
+        }
+
+        /// <summary>
+        /// Completar nombre del grupo en formato Grupo NN
+        /// </summary>
+        public override string getName()
+        {
+            return "Piratas Espaciales";
+        }
+
+        /// <summary>
+        /// Completar con la descripción del TP
+        /// </summary>
+        public override string getDescription()
+        {
+            return "MiIdea - Descripcion de la idea";
+        }
+
+
+        /// <summary>
+        /// Método que se llama una sola vez,  al principio cuando se ejecuta el ejemplo.
+        /// Escribir aquí todo el código de inicialización: cargar modelos, texturas, modifiers, uservars, etc.
+        /// Borrar todo lo que no haga falta
+        /// </summary>
+        public override void init()
+        {
+            //GuiController.Instance: acceso principal a todas las herramientas del Framework
+
+            //Device de DirectX para crear primitivas
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+
+            //Carpeta de archivos Media del alumno
+            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
+            //Creacion de una esfera
+            string sphere = GuiController.Instance.ExamplesMediaDir + "ModelosTgc\\Sphere\\Sphere-TgcScene.xml";
+            TgcSceneLoader loader = new TgcSceneLoader();
+            //Cargado de textura para el sol
+            sol = loader.loadSceneFromFile(sphere).Meshes[0];
+            sol.changeDiffuseMaps(new TgcTexture[] { TgcTexture.createTexture(d3dDevice, GuiController.Instance.ExamplesDir + "Transformations\\SistemaSolar\\SunTexture.jpg") });
+
+            //Deshabilitamos el manejo automático de Transformaciones de TgcMesh, para poder manipularlas en forma customizada
+            sol.AutoTransformEnable = false;
+            ///////////////USER VARS//////////////////
+
+            //Crear una UserVar
+            GuiController.Instance.UserVars.addVar("variablePrueba");
+
+            //Cargar valor en UserVar
+            GuiController.Instance.UserVars.setValue("variablePrueba", 5451);
+
+
+
+            ///////////////MODIFIERS//////////////////
+
+            //Crear un modifier para un valor FLOAT
+            GuiController.Instance.Modifiers.addFloat("valorFloat", -50f, 200f, 0f);
+
+            //Crear un modifier para un ComboBox con opciones
+            string[] opciones = new string[]{"opcion1", "opcion2", "opcion3"};
+            GuiController.Instance.Modifiers.addInterval("valorIntervalo", opciones, 0);
+
+            //Crear un modifier para modificar un vértice
+            GuiController.Instance.Modifiers.addVertex3f("valorVertice", new Vector3(-100, -100, -100), new Vector3(50, 50, 50), new Vector3(0, 0, 0));
+
+
+
+            ///////////////CONFIGURAR CAMARA ROTACIONAL//////////////////
+            //Es la camara que viene por default, asi que no hace falta hacerlo siempre
+            GuiController.Instance.RotCamera.Enable = true;
+            //Configurar centro al que se mira y distancia desde la que se mira
+            GuiController.Instance.RotCamera.setCamera(new Vector3(0, 0, 0), 100);
+
+
+            /*
+            ///////////////CONFIGURAR CAMARA PRIMERA PERSONA//////////////////
+            //Camara en primera persona, tipo videojuego FPS
+            //Solo puede haber una camara habilitada a la vez. Al habilitar la camara FPS se deshabilita la camara rotacional
+            //Por default la camara FPS viene desactivada
+            GuiController.Instance.FpsCamera.Enable = true;
+            //Configurar posicion y hacia donde se mira
+            GuiController.Instance.FpsCamera.setCamera(new Vector3(0, 0, -20), new Vector3(0, 0, 0));
+            */
+
+
+
+            ///////////////LISTAS EN C#//////////////////
+            //crear
+            List<string> lista = new List<string>();
+
+            //agregar elementos
+            lista.Add("elemento1");
+            lista.Add("elemento2");
+
+            //obtener elementos
+            string elemento1 = lista[0];
+
+            //bucle foreach
+            foreach (string elemento in lista)
+            {
+                //Loggear por consola del Framework
+                GuiController.Instance.Logger.log(elemento);
+            }
+
+            //bucle for
+            for (int i = 0; i < lista.Count; i++)
+            {
+                string element = lista[i];
+            }
+
+            GuiController.Instance.BackgroundColor = Color.Black;
+            currentFile = null;
+            GuiController.Instance.Modifiers.addFile("MP3-File", GuiController.Instance.ExamplesMediaDir + "Music\\I am The Money.mp3", "MP3s|*.mp3");
+        }
+
+        private void LoadMp3(string filePath)
+        {
+            if (currentFile == null || currentFile != filePath)
+            {
+                currentFile = filePath;
+
+                //Cargar archivo
+                GuiController.Instance.Mp3Player.closeFile();
+                GuiController.Instance.Mp3Player.FileName = currentFile;               
+            }
+        }
+        /// <summary>
+        /// Método que se llama cada vez que hay que refrescar la pantalla.
+        /// Escribir aquí todo el código referido al renderizado.
+        /// Borrar todo lo que no haga falta
+        /// </summary>
+        /// <param name="elapsedTime">Tiempo en segundos transcurridos desde el último frame</param>
+        public override void render(float elapsedTime)
+        {
+            //Device de DirectX para renderizar
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+            sol.Transform = TransformarSol(elapsedTime);
+            sol.render();
+
+            axisRotation += AXIS_ROTATION_SPEED * elapsedTime;
+            //Obtener valor de UserVar (hay que castear)
+            int valor = (int)GuiController.Instance.UserVars.getValue("variablePrueba");
+
+            string filePath = (string)GuiController.Instance.Modifiers["MP3-File"];
+            LoadMp3(filePath);
+
+            TgcMp3Player player = GuiController.Instance.Mp3Player;
+            TgcMp3Player.States currentState = player.getStatus();
+            GuiController.Instance.Mp3Player.play(true);
+            //Obtener valores de Modifiers
+            float valorFloat = (float)GuiController.Instance.Modifiers["valorFloat"];
+            string opcionElegida = (string)GuiController.Instance.Modifiers["valorIntervalo"];
+            Vector3 valorVertice = (Vector3)GuiController.Instance.Modifiers["valorVertice"];
+
+
+            ///////////////INPUT//////////////////
+            //conviene deshabilitar ambas camaras para que no haya interferencia
+
+            //Capturar Input teclado 
+            if (GuiController.Instance.D3dInput.keyPressed(Microsoft.DirectX.DirectInput.Key.F))
+            {
+                //Tecla F apretada
+            }
+
+            //Capturar Input Mouse
+            if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT))
+            {
+                //Boton izq apretado
+            }
+
+            //Limpiamos todas las transformaciones con la Matrix identidad
+            d3dDevice.Transform.World = Matrix.Identity;
+        }
+
+        /// <summary>
+        /// Método que se llama cuando termina la ejecución del ejemplo.
+        /// Hacer dispose() de todos los objetos creados.
+        /// </summary>
+        public override void close()
+        {
+            sol.dispose();
+        }
+
+
+        private Matrix TransformarSol(float elapsedTime)
+        {
+            Matrix scale = Matrix.Scaling(SUN_SCALE);
+            Matrix yRot = Matrix.RotationY(axisRotation);
+
+            return scale * yRot;
+        }
+    }
+}
